cfb085ff282ccfd090eadc9967beddd898725b4e
Andriy Sikora <Andriy.Sikora@gmail.com>
2016-07-24 23:12:50 +0300

    Add createRequest function

--- public/script/index.js
+++ public/script/index.js
@@ -12,23 +12,6 @@ function initPage() {
 	}
 }
 
-function createRequest() {
-	try {
-		request = new XMLHttpRequest();
-	} catch(tryMS){
-		try {
-			request = new ActiveXObject("Msxml2.XMLHTTP");
-		} catch(otherMS){
-			try {
-				request = new ActiveXObject("Microsoft.XMLHTTP");
-			} catch (failed) {
-				request = null;
-				}
-			}
-		}
-	return request;
-}
-
 function getDetails (itemName) {
 	request = createRequest();
 	if(request == null){
